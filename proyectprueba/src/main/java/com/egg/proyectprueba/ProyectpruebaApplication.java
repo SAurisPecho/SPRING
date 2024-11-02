8ef2624ca4b435c1cc9c58886f6bb2579e939854
“Sarai <auris.sarai172@gmail.com>
2024-11-01 21:24:54 -0500

    Clase1: Primer proyecto & Proyecto biblioteca

---
+++ proyectprueba/src/main/java/com/egg/proyectprueba/ProyectpruebaApplication.java
@@ -0,0 +1,13 @@
+package com.egg.proyectprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectpruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectpruebaApplication.class, args);
+	}
+
+}
---
+++ proyectprueba/src/test/java/com/egg/proyectprueba/ProyectpruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.proyectprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectpruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
